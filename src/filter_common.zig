cad3d64d1e1a6475e17b0ed5d357de551dccb427
Duncan <59705125+DuncanRuns@users.noreply.github.com>
2026-01-17 21:03:46 -0400

    Fix melon slices, standardize w/ nextCount, v4.1.1

--- src/filter_common.zig
+++ src/filter_common.zig
@@ -163,31 +163,44 @@ pub fn getShipwreckTreasureLoot(seed: u64, block_x: c_int, block_z: c_int, salt:
     var gold_nuggets: u8 = 0;
     var emeralds: u8 = 0;
 
-    // Pool 1 (w150): 3-6 uni rolls, w90 1-5 iron_ingot, w10 1-5 gold_ingot, w40 1-5 emerald, w5 1 diamond, w5 1 experience bottle
+    // Pool 1 (w150): 3-6 uni rolls
+    // w90 1-5 iron_ingot
+    // w10 1-5 gold_ingot
+    // w40 1-5 emerald
+    // w5 1 diamond
+    // w5 1 experience bottle
     {
-        const max = cubiomes.nextInt(&r, 4) + 3;
+        const max = nextCount(&r, 3, 6);
         var i: i32 = 0;
         while (i < max) : (i += 1) {
             const choice = cubiomes.nextInt(&r, 150);
             if (choice < 90) {
-                iron += @intCast(cubiomes.nextInt(&r, 5) + 1);
+                // 1-5 iron_ingot
+                iron += @intCast(nextCount(&r, 1, 5));
             } else if (choice < 100) {
-                gold += @intCast(cubiomes.nextInt(&r, 5) + 1);
+                // 1-5 gold_ingot
+                gold += @intCast(nextCount(&r, 1, 5));
             } else if (choice < 140) {
-                emeralds += @intCast(cubiomes.nextInt(&r, 5) + 1);
+                // 1-5 emerald
+                emeralds += @intCast(nextCount(&r, 1, 5));
             } else if (choice < 145) {
+                // 1 diamond
                 diamonds += 1;
             } // else 1 experience bottle
         }
     }
 
-    // Pool 2 (w80): 2-5 uni rolls, w50 1-10 iron_nugget, w10 1-10 gold_nugget, w20 1-10 lapis_lazuli
+    // Pool 2 (w80): 2-5 uni rolls
+    // w50 1-10 iron_nugget
+    // w10 1-10 gold_nugget
+    // w20 1-10 lapis_lazuli
     {
-        const max = cubiomes.nextInt(&r, 4) + 2;
+        const max = nextCount(&r, 2, 5);
         var i: i32 = 0;
         while (i < max) : (i += 1) {
             const choice = cubiomes.nextInt(&r, 80);
-            const amount = cubiomes.nextInt(&r, 10) + 1; // All entries in this pool are 1-10
+            // All entries in this pool are 1-10
+            const amount = nextCount(&r, 1, 10);
             if (choice < 50) {
                 iron_nuggets += @intCast(amount);
             } else if (choice < 70) {
@@ -235,41 +248,41 @@ pub fn getShipwreckSupplyLoot(seed: u64, block_x: c_int, block_z: c_int, salt: u
     // w3 leather_chestplate + enchant_randomly
     // w3 leather_leggings + enchant_randomly
     // w3 leather_boots + enchant_randomly
-    const max = cubiomes.nextInt(&r, 8) + 3;
+    const max = nextCount(&r, 3, 10);
     var i: i32 = 0;
     while (i < max) : (i += 1) {
         const choice = cubiomes.nextInt(&r, 77);
         if (choice < 8) {
-            // std.debug.print("Paper\n", .{});
-            _ = cubiomes.nextInt(&r, 12);
+            // 1-12 paper
+            disposeNextCount(&r, 1, 12);
         } else if (choice < 22) {
-            // std.debug.print("Potato\n", .{});
-            _ = cubiomes.nextInt(&r, 5);
+            // 2-6 (poisonous_)potato
+            disposeNextCount(&r, 2, 6);
         } else if (choice < 29) {
-            // std.debug.print("Carrot\n", .{});
-            carrots += @intCast(cubiomes.nextInt(&r, 5) + 4);
+            // 4-8 carrot
+            carrots += @intCast(nextCount(&r, 4, 8));
         } else if (choice < 36) {
-            // std.debug.print("wheat\n", .{});
-            wheat += @intCast(cubiomes.nextInt(&r, 14) + 8);
+            // 8-21 wheat
+            wheat += @intCast(nextCount(&r, 8, 21));
         } else if (choice < 46) {
             // std.debug.print("Stew\n", .{});
             // Discard effect calculation
             cubiomes.skipNextN(&r, 2);
         } else if (choice < 52) {
-            // std.debug.print("Coal\n", .{});
-            _ = cubiomes.nextInt(&r, 7);
+            // 2-8 coal
+            disposeNextCount(&r, 2, 8);
         } else if (choice < 57) {
-            // std.debug.print("Flesh\n", .{});
-            rotten_flesh += @intCast(cubiomes.nextInt(&r, 20) + 5);
+            // 5-24 rotten_flesh
+            rotten_flesh += @intCast(nextCount(&r, 5, 24));
         } else if (choice < 61) {
-            // std.debug.print("Pump/Bamb\n", .{});
-            _ = cubiomes.nextInt(&r, 3);
+            // 1-3 pumpkin/bamboo
+            disposeNextCount(&r, 1, 3);
         } else if (choice < 64) {
-            // std.debug.print("Gunpowder\n", .{});
-            gunpowder += @intCast(cubiomes.nextInt(&r, 5) + 1);
+            // 1-5 gunpowder
+            gunpowder += @intCast(nextCount(&r, 1, 5));
         } else if (choice < 65) {
-            // std.debug.print("Tnt\n", .{});
-            tnt += @intCast(cubiomes.nextInt(&r, 2) + 1);
+            // 1-2 tnt
+            tnt += @intCast(nextCount(&r, 1, 2));
         } else if (choice < 68) {
             // std.debug.print("Helmet\n", .{});
             _ = switch (cubiomes.nextInt(&r, 11)) {
@@ -322,33 +335,45 @@ pub fn getBTLoot(seed: u64, chunk_x: c_int, chunk_z: c_int, salt: u32) BuriedTre
     var emeralds: u8 = 0;
 
     // Pool 1 (no rng)
-    // Pool 2: 5-8 uni rolls, w20 iron, w10 gold, w5 tnt
+    // Pool 2: 5-8 uni rolls,
+    // w20 1-4 iron
+    // w10 1-4 gold
+    // w5 1-2 tnt
     {
-        const max = cubiomes.nextInt(&rand, 4) + 5;
+        const max = nextCount(&rand, 5, 8);
         var i: i32 = 0;
         while (i < max) : (i += 1) {
             const choice = cubiomes.nextInt(&rand, 35);
             if (choice < 20) {
-                iron += @intCast(cubiomes.nextInt(&rand, 4) + 1);
+                // 1-4 iron_ingot
+                iron += @intCast(nextCount(&rand, 1, 4));
             } else if (choice < 30) {
-                gold += @intCast(cubiomes.nextInt(&rand, 4) + 1);
+                // 1-4 gold_ingot
+                gold += @intCast(nextCount(&rand, 1, 4));
             } else {
-                tnt += @intCast(cubiomes.nextInt(&rand, 2) + 1);
+                // 1-2 tnt
+                tnt += @intCast(nextCount(&rand, 1, 2));
             }
         }
     }
-    // Pool 3: 1-3 uni rolls, w5 emerald, w5 diamond, w5 prismarine crystals
+    // Pool 3: 1-3 uni rolls
+    // w5 4-8 emerald
+    // w5 1-2 diamond
+    // w5 1-5 prismarine crystals
     {
-        const max = cubiomes.nextInt(&rand, 3) + 1;
+        const max = nextCount(&rand, 1, 3);
         var i: i32 = 0;
         while (i < max) : (i += 1) {
             const choice = cubiomes.nextInt(&rand, 15);
             if (choice < 5) {
-                emeralds += @intCast(cubiomes.nextInt(&rand, 5) + 4);
+                // 4-8 emerald
+                emeralds += @intCast(nextCount(&rand, 4, 8));
             } else if (choice < 10) {
-                diamonds += @intCast(cubiomes.nextInt(&rand, 2) + 1);
+                // 1-2 diamond
+                diamonds += @intCast(nextCount(&rand, 1, 2));
             } else {
-                _ = cubiomes.next(&rand, 31);
+                // 1-5 prismarine crystals
+                disposeNextCount(&rand, 1, 5);
             }
         }
     }
@@ -413,20 +438,25 @@ pub fn getDesertPyramidSingleChestLoot(chest_seed: u64) DesertTempleLoot {
 
     var loot: DesertTempleLoot = .{};
     {
-        const max = cubiomes.nextInt(&rand, 3) + 2;
+        const max = nextCount(&rand, 2, 4);
         var i: i32 = 0;
         while (i < max) : (i += 1) {
             const choice = cubiomes.nextInt(&rand, 232);
             if (choice < 5) {
-                loot.diamonds += @intCast(cubiomes.nextInt(&rand, 3) + 1);
+                // 1-3 diamond
+                loot.diamonds += @intCast(nextCount(&rand, 1, 3));
             } else if (choice < 20) {
-                loot.iron += @intCast(cubiomes.nextInt(&rand, 5) + 1);
+                // 1-5 iron_ingot
+                loot.iron += @intCast(nextCount(&rand, 1, 5));
             } else if (choice < 35) {
-                loot.gold += @intCast(cubiomes.nextInt(&rand, 6) + 2);
+                // 2-7 gold_ingot
+                loot.gold += @intCast(nextCount(&rand, 2, 7));
             } else if (choice < 100) {
-                _ = cubiomes.nextInt(&rand, 3); // emerald, bone, spider_eye
+                // 1-3 emerald, 4-6 bone, 1-3 spider_eye
+                disposeNextCount(&rand, 1, 3); // +3 for bone, no difference to rng disposal
             } else if (choice < 125) {
-                _ = cubiomes.nextInt(&rand, 5); // rotten_flesh
+                // 3-7 rotten_flesh
+                disposeNextCount(&rand, 3, 7);
             } else if (choice < 175) {
                 // Horse item
             } else if (choice < 195) {
@@ -480,6 +510,7 @@ pub fn getDesertPyramidSingleChestLoot(chest_seed: u64) DesertTempleLoot {
             } else if (choice < 215) {
                 // golden_apple
             } else if (choice < 217) {
+                // enchanted_golden_apple
                 loot.enchanted_golden_apples += 1;
             } // else nothing
         }
@@ -531,25 +562,31 @@ pub fn getRuinedPortalLoot(seed: u64, block_x: c_int, block_z: c_int, salt: u32)
 
     var loot: RuinedPortalLoot = .{};
 
-    const max = cubiomes.nextInt(&rand, 5) + 4;
+    const max = nextCount(&rand, 4, 8);
     var i: i32 = 0;
     while (i < max) : (i += 1) {
         const choice = cubiomes.nextInt(&rand, 398);
 
         if (choice < 40) {
-            loot.obsidian += @intCast(cubiomes.nextInt(&rand, 2) + 1);
+            // 1-2 obsidian
+            loot.obsidian += @intCast(nextCount(&rand, 1, 2));
         } else if (choice < 80) {
-            loot.flint += @intCast(cubiomes.nextInt(&rand, 4) + 1);
+            // 1-4 flint
+            loot.flint += @intCast(nextCount(&rand, 1, 4));
         } else if (choice < 120) {
-            loot.iron_nuggets += @intCast(cubiomes.nextInt(&rand, 10) + 9);
+            // 9-18 iron_nugget
+            loot.iron_nuggets += @intCast(nextCount(&rand, 9, 18));
         } else if (choice < 160) {
+            // flint_and_steel
             loot.flint_and_steels += 1;
         } else if (choice < 200) {
+            // fire_charge
             loot.fire_charges += 1;
         } else if (choice < 215) {
             // golden apple
         } else if (choice < 230) {
-            _ = cubiomes.nextInt(&rand, 21);
+            // 4-24 gold_nugget
+            disposeNextCount(&rand, 4, 24);
         } else if (choice < 245) {
             // golden sword, enchant_randomly
             // 0: minecraft:sharpness, 5
@@ -676,27 +713,38 @@ pub fn getRuinedPortalLoot(seed: u64, block_x: c_int, block_z: c_int, salt: u32)
                 else => 0,
             };
         } else if (choice < 370) {
-            // glistering melon slices
+            // 4-12 melon slice
+            disposeNextCount(&rand, 4, 12);
         } else if (choice < 375) {
             // golden horse armor
         } else if (choice < 380) {
             // light weighted pressure plate
         } else if (choice < 385) {
-            _ = cubiomes.nextInt(&rand, 9);
+            // 4-12 golden_carrot
+            disposeNextCount(&rand, 4, 12);
         } else if (choice < 390) {
             // clock
         } else if (choice < 395) {
-            _ = cubiomes.nextInt(&rand, 7);
+            // 2-8 gold_ingot
+            disposeNextCount(&rand, 2, 8);
         } else if (choice < 396) {
             // bell
         } else if (choice < 397) {
             // enchanted golden apple
         } else {
             // 1-2 gold blocks
-            _ = cubiomes.nextInt(&rand, 2);
+            disposeNextCount(&rand, 1, 2);
         }
     }
     return loot;
+}
+
+inline fn nextCount(rand: *u64, min: c_int, max: c_int) c_int {
+    return cubiomes.nextInt(rand, max - min + 1) + min;
+}
+
+inline fn disposeNextCount(rand: *u64, min: c_int, max: c_int) void {
+    _ = cubiomes.nextInt(rand, max - min + 1);
 }
 
 pub fn getLavaLake(world_seed: u64, block_x: c_int, block_z: c_int, salt: u32) ?cubiomes.Pos3 {
